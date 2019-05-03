8ef2322450176c2a043b48fd7f25ebc270c48e27
chris <christopherparello@gmail.com>
2019-05-02 21:26:53 -0700

    polish powerups

--- Source/CoopGame/Public/SPickupActor.h
+++ Source/CoopGame/Public/SPickupActor.h
@@ -32,7 +32,7 @@ protected:
 
 	ASPowerpActor* PowerUpInstance;
 
-	UPROPERTY(EditDefaultsOnly, Category = "PickupActor")
+	UPROPERTY(EditInstanceOnly, Category = "PickupActor")
 	float CooldownDuration;
 
 	FTimerHandle TimerHandle_RespawnTimer;
